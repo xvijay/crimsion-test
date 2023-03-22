122aa6ec2cae4a32d12b8cf3e8ca7ca872df951b
Vijay Tripathi <vijayxtri@gmail.com>
2023-03-22 16:48:40 +0530

    complete

--- src/App.js
+++ src/App.js
@@ -1,24 +1,106 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+import "./App.css";
+
+const Homepage = () => {
+  const [apiData, setApiData] = useState([]);
+
+  useEffect(() => {
+    fetch("https://dummyjson.com/products")
+      .then((response) => response.json())
+      .then((data) => setApiData(data.products));
+  }, []);
+  useEffect(() => {
+    console.log(apiData.products);
+  });
+
+  return (
+    <>
+      <div className="main_title">Knowledge Base</div>
+      <hr className="main_title_hr"></hr>
+      <div className="main_container">
+        {apiData.map((item, index) => {
+          console.log(item);
+          return (
+            <div className="card-container" key={index}>
+              <div>
+                <img src={item.images[0]} />
+              </div>
+              <div style={{ padding: "1rem" }}>
+                <div
+                  className=""
+                  style={{ display: "flex", justifyContent: "space-between" }}
+                >
+                  <div>Brand : {item.brand}</div>
+                  <div>{item.category}</div>
+                </div>
+                <div className="card_title">{item.title}</div>
+                <div>{item.description}</div>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </>
+  );
+};
+
+const Timer = () => {
+  const [timer, setTimer] = useState(0);
+  const [intervals, setIntervals] = useState();
+  const [pause, setPause] = useState(false);
+
+  const startTimer = (timer) => {
+    const timerCounter = setInterval(() => {
+      setTimer((prevCount) => prevCount + 1);
+    }, 1000);
+    setIntervals(timerCounter);
+  };
+
+  const resetTimer = () => {
+    setTimer(0);
+    clearInterval(intervals);
+  };
+  const pauseTimer = () => {
+    clearInterval(intervals);
+  };
+
+  return (
+    <>
+      <div>{timer}</div>
+      <div>
+        <button onClick={() => startTimer()}>Start</button>
+        <button
+          onClick={() => {
+            pauseTimer();
+          }}
+        >
+          pause
+        </button>
+        <button
+          onClick={() => {
+            resetTimer();
+          }}
+        >
+          reset
+        </button>
+      </div>
+    </>
+  );
+};
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Homepage></Homepage>}></Route>
+          <Route path="/timer" element={<Timer></Timer>}></Route>
+        </Routes>
+        {/* 
+        <Homepage></Homepage> */}
+      </BrowserRouter>
     </div>
   );
 }
